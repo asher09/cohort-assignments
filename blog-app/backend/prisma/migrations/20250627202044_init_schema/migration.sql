9074af3062e67cc2cb81528405171ac02bebccd3
Turbobot <turbobot@vercel.com>
2025-07-13 17:44:47 +0530

    feat(create-turbo): apply official-starter transform

--- blog-app/backend/prisma/migrations/20250627202044_init_schema/migration.sql
+++ blog-app/backend/prisma/migrations/20250627202044_init_schema/migration.sql
@@ -20,7 +20,7 @@ CREATE TABLE "Post" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("username");
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
